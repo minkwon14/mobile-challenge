4a0202970e5f4db93724ce93759fb76316cdc9a4
minkwon14 <min.kwon14@gmail.com>
2017-12-18 17:52:25 -0500

    Create Project

---
+++ 500pxGallery/500pxGallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  500pxGallery
+//
+//  Created by Minseo Kwon on 2017-12-18.
+//  Copyright © 2017 MinKwon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
